a506c773d7ef037a0a3b7d4ef2f94f0d43c2ed52
ahmadraza-mlbench <ahmad_raza@ml-bench.com>
2025-12-18 16:55:23 +0500

    bug fixes

--- app/components/landing/Section3.tsx
+++ app/components/landing/Section3.tsx
@@ -263,7 +263,7 @@ const TextBox = ({
       <h3 className="text-[20px] lg:text-[24px] font-semibold text-black mt-5">
         {title}
       </h3>
-      <span className="text-sm lg:text-[18px] font-normal text-black text-start">
+      <span className="text-base! md:text-sm! lg:text-[18px]! font-normal text-black text-start">
         {text}
       </span>
     </div>
